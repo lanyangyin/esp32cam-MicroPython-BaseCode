0cdc4b77224fd3da651fe686dfb48b0360efacd2
lanyangyin <2436725966@qq.com>
2026-08-16 13:40:39 +0800

    ing

--- photo/__init__.py
+++ photo/__init__.py
@@ -6,10 +6,14 @@ from .photo_capturer import PhotoCapturer
 from .smart_photo_taker import take_smart_photo
 from .downgrade_capture import take_photo_with_downgrade
 from .manual_photo_taker import take_photo_manual
+from .gray_quick import gray_quick_capture          # 新增
+from .gray_analyzer import gray_analyzer_capture    # 新增
 
 __all__ = [
     "PhotoCapturer",
     "take_smart_photo",
     "take_photo_with_downgrade",
     "take_photo_manual",
+    "gray_quick_capture",
+    "gray_analyzer_capture",
 ]
